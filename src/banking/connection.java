8a1817899f862c696da93b4d185f8132bff34e94
Vinay Kumar Soni <vinaykumarsoni449@gmail.com>
2025-09-27 10:51:25 +0530

    Update connection.java

--- src/banking/connection.java
+++ src/banking/connection.java
@@ -3,21 +3,18 @@ package banking;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-// Global connection Class
 public class connection {
     static Connection con; 
     public static Connection getConnection()
     {
         try {
-            String mysqlJDBCDriver
-                    = "com.mysql.cj.jdbc.Driver"; 
-            String url
-                    = "jdbc:mysql://localhost:3306/BANK"; 
-            String user = "root";       
-            String pass = "vinay_kumar@&1976";  
+            String mysqlJDBCDriver = "com.mysql.cj.jdbc.Driver"; 
+            String url = "jdbc:mysql://localhost:3306/BANK"; 
+            String user = System.getenv("DB_USER");  
+            String pass = System.getenv("DB_PASS");  
+
             Class.forName(mysqlJDBCDriver);
-            con = DriverManager.getConnection(url, user,
-                    pass);
+            con = DriverManager.getConnection(url, user, pass);
         }
         catch (Exception e) {
             System.out.println("Connection Failed!");
